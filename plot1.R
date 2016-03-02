6cfa01c84ffe08ec465d6db1ba910a1937d3ff1d
downtownadam <the.adam.blair@gmail.com>
2016-03-01 21:24:13 -0600

    Added files via upload

--- plot1.R
+++ plot1.R
@@ -6,5 +6,5 @@ power$DateTime <- strptime(paste(power$Date,power$Time),"%d/%m/%Y %H:%M:%S")
 power<- power[power$Date=="1/2/2007" | power$Date=="2/2/2007",]
 
 png(file="plot1.png",width=480,height=480)
-hist(power$Global_active_power,col="orange",main="Global Active Power",xlab="Global Active Power (Kilowatts)")
+hist(power$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (Kilowatts)")
 dev.off()
